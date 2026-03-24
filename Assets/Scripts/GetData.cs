37fe617754fc7180f609ca692ab71c3b6037ca84
AliceMC06 <Alice3.Chilcott@live.uwe.ac.uk>
2026-03-24 13:03:29 +0000

    add API pt1

---
+++ Assets/Scripts/GetData.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using SimpleJSON;
+
+public class GetData : MonoBehaviour
+{
+    public string DataURL;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        StartCoroutine(getData());
+    }
+
+    IEnumerator getData()
+    {
+        using (UnityWebRequest request = UnityWebRequest.Get(DataURL))
+        {
+            yield return request.SendWebRequest();
+
+            if (request.result == UnityWebRequest.Result.ConnectionError)
+            {
+                Debug.LogError(request.error);
+            }
+            else
+            {
+                string json = request.downloadHandler.text;
+                Debug.Log(json);
+            }
+        }
+    }
+
+
+}
